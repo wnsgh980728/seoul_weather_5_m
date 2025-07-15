d63327ba2eb798abd202196ab84c6566fd885f86
wnsgh980728 <xkdlrjwns3@naver.com>
2025-07-15 13:26:51 +0900

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,11 +6,11 @@ import os                     # 폴더 생성
 
 from datetime import datetime # 시간 변환
 
-API_KEY = os.getenv("API_KEY_W")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
 city = "seoul"
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric"
 
 response = requests.get(url)
 
